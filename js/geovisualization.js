17897509d3fff6d607689ac1aa2ca66aff649ba0
Xinyu Wu <xinyuwu421@gmail.com>
2022-04-14 09:38:12 -0400

    Update geovisualization.js

--- js/geovisualization.js
+++ js/geovisualization.js
@@ -1,39 +1,26 @@
+// Import data from csv
 d3.csv("data/cleaned_nutrition_df.csv").then((data) => {
 
-
+  // Create chart and define size and margins
   let svg = d3.select("#map-id"),
             width1 = +svg.attr("width"),
             height1 = +svg.attr("height"),
             margin1 = {top: 50, right: 100, bottom: 50, left: 50};
 
-  // Map and projection
+  // Define map type and projection
   let path = d3.geoPath();
-
   let projection= d3.geoMercator()
                     .scale(130)
                     .center([75, 0])
                     .translate([width1 / 2, height1 /2]);
 
-  // Data and color scale
+  // Data and color scale for map
   let data1 = new Map()
   let colorScale = d3.scaleThreshold()
                      .domain([0, 1, 2, 3, 4, 5, 6])
                      .range(d3.schemeReds[7]);
-
-  // const zoom = d3.zoom().scaleExtent([1, 8]).on("zoom", zoomed);
-  //
-  // svg.call(zoom);
-  //
-  // function zoomed(event) {
-  // const {transform} = event;
-  // svg.attr("transform", transform);
-  // svg.attr("stroke-width", 1 / transform.k);}
-
-
-
-
-
-  // Load external data
+  
+  // Load external geojson data for map, return selected features from csv dataset
   let promises = []
   promises.push(d3.json("geo-countries/data/countries.geojson"))
   promises.push(d3.csv("data/cleaned_nutrition_df.csv", function(d) { data1.set(d.country_code, +d.cost_nutrition); }))
@@ -41,7 +28,7 @@ d3.csv("data/cleaned_nutrition_df.csv").then((data) => {
   myDataPromises = Promise.all(promises).then(function(mydata) {
   let topo = mydata[0]
 
-  // tooltip
+  // Create tooltip
   let tooltip1 = d3.select("#vis-map")
       .append("div")
       .style("opacity", 0)
@@ -53,10 +40,11 @@ d3.csv("data/cleaned_nutrition_df.csv").then((data) => {
       .style("margin-right", "732px")
 
 
-  // legend
+  // Define legend size
   const legend_x = width1 - margin1.left - 250
   const legend_y = height1 - 400
 
+  // Create legend for map
   svg.append("g")
       .attr("class", "legendQuant")
       .attr("transform", "translate(" + legend_x + "," + legend_y+")")
@@ -70,7 +58,8 @@ d3.csv("data/cleaned_nutrition_df.csv").then((data) => {
   svg.select(".legendQuant")
       .call(legendLinear)
 
-
+  
+   // Define interactive functions
   let mouseOver = function(d, event) {
       d3.selectAll(".country_name")
           .transition()
@@ -79,8 +68,6 @@ d3.csv("data/cleaned_nutrition_df.csv").then((data) => {
           .transition()
           .duration(200)
           .style("stroke", "black")
-
-      // tooltip
       tooltip1
           .style("opacity", 1)
           .style("visibility", "visible")
@@ -96,38 +83,33 @@ d3.csv("data/cleaned_nutrition_df.csv").then((data) => {
       d3.selectAll(".country_name")
           .transition()
           .duration(200)
-          // .style("opacity", .8)
       d3.select(this)
           .transition()
           .duration(200)
           .style("stroke", "transparent")
-
-      // tooltip
       tooltip1
           .transition()
           .duration(200)
           .style("opacity", 1)
   }
 
-  // Draw the map
+  // Draw the world map
   svg.append("g")
       .selectAll("path")
       .data(topo.features)
       .enter()
       .append("path")
-
-
-      // draw each country
       .attr("d", d3.geoPath()
           .projection(projection)
       )
 
-      // set the color of each country
+      // Define the color for each country based off of the cost of diet
       .attr("fill", function (d) {
           d.cost_nutrition = data1.get(d.properties.ISO_A3) || 0;
           return colorScale(d.cost_nutrition);
       })
-
+      
+      // Define styling for mouse interactions
       .style("stroke", "transparent")
       .attr("class", function(d) { return d.properties.ADMIN } )
       .style("opacity", .8)
