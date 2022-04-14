f053867b4eb2171d98820c35922dede99d3108fc
Xinyu Wu <xinyuwu421@gmail.com>
2022-04-14 09:34:26 -0400

    Update barchart.js

--- js/barchart.js
+++ js/barchart.js
@@ -1,15 +1,16 @@
-//Bar Chart
-
+// Bar chart specifications
 const widthbar = 1000;
 const heightbar = 2500;
 const marginbar = {left:150, right:50, bottom:50, top:50};
 const yTooltipOffset2 = 15;
 
+// Create legend for bar chart with specified colors
 const svg10 = d3.select("#bar-legend")
                 .append("svg")
                 .attr("width", 450)
                 .attr("height", 260);
 
+// Colors and legend formatting
 svg10.append("text")
     .attr("x", 190)
     .attr("y", 20)
@@ -86,6 +87,7 @@ svg10.append("text")
     .text("$6 - $7 USD")
     .style("font-size", "15px")
 
+// Create bar chart with pre-defined size (from above)
 var svg1 = d3.select("#csv-bar")
               .append("svg")
               .attr("width", widthbar+marginbar.left+marginbar.right)
@@ -94,71 +96,60 @@ var svg1 = d3.select("#csv-bar")
               .style("margin-top", "10px")
               .attr("transform", "translate("+marginbar.left + "," + marginbar.top + ")");
 
+// Add x-axis
+let x = d3.scaleLinear()
+          .range([0, widthbar]);
+let xAxis = svg1.append("g")
+                .attr("class", "myXaxis")
 
-      // Add x-axis
-      let x = d3.scaleLinear()
-                .range([0, widthbar]);
-      let xAxis = svg1.append("g")
-                      .attr("class", "myXaxis")
+ // Add y-axis
+let y = d3.scaleBand()
+          .range([0, heightbar])
+          .padding(.1);
+let yAxis = svg1.append("g")
+                .attr("transform", "translate(0," + heightbar + ")")
+let title;
 
-       // Add y-axis
-      let y = d3.scaleBand()
-                .range([0, heightbar])
-                // .domain(data.map(function(d) {return d.country_name;}))
-                .padding(.1);
-      let yAxis = svg1.append("g")
-                      .attr("transform", "translate(0," + heightbar + ")")
-      let title;
-
+// Create tool tip to change opacity when data filter is selected
 const tooltip1 = d3.select("#csv-bar")
                     .append("div")
                     .attr("id", "tooltip1")
                     .select("opacity", 0)
                     .attr("class", "tooltip");
 
-      function update(selectedVar) {
+// Function to select and filter data that is shown in bar chart
+function update(selectedVar) {
 
-d3.csv("data/clean_nutrition_df.csv").then((data) => {
-            y.domain(data.map(function(d) {return d.country_name; }))
-            // yAxis.transition().duration(1000).call(d3.axisLeft(y))
+  d3.csv("data/clean_nutrition_df.csv").then((data) => {
+    y.domain(data.map(function(d) {return d.country_name; }))
+    x.domain([0, d3.max(data, function(d) {return +d[selectedVar] }) ]);
+    xAxis.transition().duration(1000).call(d3.axisTop(x));
 
-            x.domain([0, d3.max(data, function(d) {return +d[selectedVar] }) ]);
-            xAxis.transition().duration(1000).call(d3.axisTop(x));
-
-
-
-
-var mouseover1 = function(event, d) {
+  var mouseover1 = function(event, d) {
     tooltip1.html("Country:" + d.country_name + "<br>" + function(d) {return (d[selectedVar])} + "<br>")
             .style("opacity", 1);
 };
 
 
-
+// User interaction definitions
 const mousemove1 = function(event, d) {
     tooltip1.style("left", (event.pageX) + "px")
     .style("bottom", (event.pageY + yTooltipOffset2) + "px");
 }
 
 
-
 const mouseleave1 = function(event, d) {
     tooltip1.style("opacity", 0);
 }
 
-// bar color scale
+// Bar chart color definitions
 let colors = d3.scaleThreshold()
                    .domain([0, 1, 2, 3, 4, 5, 6, 7])
                    .range(["#FFFFFF","#ffbaba","#ff7b7b", "#ff5252", "#ff0000", "#a70000", "#641E16", "#000000"]);
 
-                   // const color = d3.scaleOrdinal()
-                   //                 .domain(["High-income", "Upper-middle", "Lower-middle", "Low-income"])
-                   //                 .range(["#A5D6A7", "#D1C4E9", "#B2EBF2", "#FFCDD2"])
-
+// Create bar chart
 let bars = svg1.selectAll("rect")
                         .data(data)
-
-
             bars.enter()
              .append("rect")
              .merge(bars)
@@ -171,8 +162,7 @@ let bars = svg1.selectAll("rect")
              .style("fill", function(d) {return colors(d[selectedVar]); })
 
 
-
-            svg1.append("g")
+          svg1.append("g")
             .attr("class", "y axis")
             .call(d3.axisLeft(y));
 
@@ -182,57 +172,21 @@ let bars = svg1.selectAll("rect")
             .on("mousemove", mousemove1)
             .on("mouseleave", mouseleave1);
 
-             let text = svg1.append("text")
-                            .data(data)
-
-                            text
-                            .exit()
-                            .remove()
-                            .transition()
-                            .duration(1000)
-                            .attr("x", function(d) {return x(d[selectedVar]);})
-                            .attr("y", y.bandwidth())
-                            .text(function(d) {
-                              return d[selectedVar]
-                            })
-                            .style("fill", "black")
-
-
-
-         // let text1 = svg1.append("text")
-         //  .attr("x", (widthbar / 2))
-         //   .attr("y", 0 - (marginbar.top / 2))
-         //    .attr("text-anchor", "middle")
-         //    .style("font-size", "16px")
-         //
-         //    text1.merge(text1)
-         //    .transition()
-         //    .duration(1000)
-         //    .text(selectedVar);
-
-            // svg1
-            // .append("text")
-            // .data(data)
-            // .attr("x", function(d) {return x(d[selectedVar]);})
-            // .attr("y", y.bandwidth())
-            // .text(function(d) {
-            //   return d[selectedVar];
-            // })
-            // .style("fill", "black")
-        })
+//  Depict countries corresponding to data
+ let text = svg1.append("text")
+                .data(data)
+                text
+                .exit()
+                .remove()
+                .transition()
+                .duration(1000)
+                .attr("x", function(d) {return x(d[selectedVar]);})
+                .attr("y", y.bandwidth())
+                .text(function(d) {
+                  return d[selectedVar]
+                })
+                .style("fill", "black")
+          })
       }
 
       update('cost_energy')
-
-      // svg1.selectAll(".bar")
-      //       .data(data)
-      //       .enter()
-      //       .append("rect")
-      //       .attr("class", "bar")
-      //       .attr("x", 0)
-      //       .attr("height", y.bandwidth())
-     //       .attr("y", function(d) {return y(d.country_name);})
-      //       .attr("width", function(d) {return x(d.cost_nutrition);})
-      //       .on("mouseover", mouseover1)
-      //       .on("mousemove", mousemove1)
-      //       .on("mouseleave", mouseleave1);
